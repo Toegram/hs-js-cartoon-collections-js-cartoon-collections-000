9286cfd126545c046e2242b01859c82bc309b5bb
Anthony Turetsky <tturetsky@gmail.com>
2018-05-05 22:09:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function dwarfRollCall(dwarves) {
+  let dwarvesArray = []
+  for (let dwarvesIndex = 0; dwarvesIndex < dwarves.length; dwarvesIndex++);
+  dwarvesArray.push(`${dwarvesIndex + 1}. ${dwarves[dwarvesIndex]}`)
+  return `${dwarvesArray}`
+  console.log(dwarvesArray)
 }
 
 function summonCaptainPlanet(planeteerCalls){
