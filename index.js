295e874507d7225928a0041c11e755a6e45ecb14
Anthony Turetsky <tturetsky@gmail.com>
2018-05-06 01:09:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 function dwarfRollCall(dwarves) {
-  let dwarvesArray = []
-  for (let dwarvesIndex = 0; dwarvesIndex < dwarves.length; dwarvesIndex++);
-  dwarvesArray.push(`${dwarvesIndex + 1}. ${dwarves[dwarvesIndex]}`)
-  return `${dwarvesArray}`
-  console.log(dwarvesArray)
+  let dwarvesArray = [];
+  for (let i = 0; i < dwarves.length; i++) {
+  dwarvesArray.push(`${i + 1}. ${dwarves[i]}`);
+}
+ return dwarvesArray.join(' ');
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  let planeteerArray = [];
+  for (let i = 0; i < planeteerCalls.length; i++) {
+    planeteerArray.push(`${i.toUpperCase}!`)
+  }
+  return planeteerArray;
 }
 
 function longPlaneteerCalls(words) {
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
-function findTheCheese (foods) {
+function findTheCheese(foods) {
+   var iHazCheese = ["cheddar"]
+  for (var i = 0; i < foods.length; i++) {
+    for (var j = 0; j < iHazCheese.length; j++) {
+    if (foods[i] === iHazCheese[j]) {
+      return foods[i];
+    } 
+    }
+  } 
+  return 'no cheese' 
 }
